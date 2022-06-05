758d6bddf17b3ffab68cde2ada23f211789faf05
cxxi <l.a_lumet@yahoo.fr>
2022-06-05 14:08:03 +0200

    Initial Commit 5

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,9 @@
+
+import commonjs from '@rollup/plugin-commonjs';
+import noderesolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
 export default {
 	input: `src/index.js`,
 	output: [
@@ -14,5 +20,16 @@ export default {
 			file: 'dist/index.umd.js',
 			format: 'umd'
 		},
-	]
-}
+		{
+        	format: 'iife',
+        	file: 'dist/index.iife.js',
+        	name: 'JellycatComponent'
+    	}
+	],
+    plugins: [
+        commonjs(),
+        noderesolve(),
+        babel({ babelHelpers: 'bundled' }),
+        terser()
+    ]
+};
