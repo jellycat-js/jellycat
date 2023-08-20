cce897b1712ac1ac08f9198afe250fa70197137c
cxxi <l.a_lumet@yahoo.fr>
2023-08-20 05:11:44 +0200

    DEV - simplify lifecycle fn

--- src/index.js
+++ src/index.js
@@ -115,7 +115,7 @@ mixins.lifeCycling = function(superclass)
 		async _runStep(lifeCycle)
 		{
 			if (['down', 'up'].includes(lifeCycle)) return true
-				
+			console.log(lifeCycle)
 			return new Promise(async (resolve, reject) => {
 				const args = await this[`_${lifeCycle}`]()
 				resolve(this.methods.includes(lifeCycle) ? await this[lifeCycle](...args) : true)
