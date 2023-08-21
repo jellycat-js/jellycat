334d0fa01ad88893e05dbe06e02534f32890b769
cxxi <l.a_lumet@yahoo.fr>
2023-08-21 15:12:31 +0200

    DEV - Test about a JcAppComponent

--- src/index.js
+++ src/index.js
@@ -330,6 +330,11 @@ const App = function(superclass)
 
 		async __init(args)
 	    {
+	    	this.router.routes = routes
+	    	console.log(this.router.resolve(window.location.pathname))
+			// this.navigate = this.navigate.bind(this)
+			// this._view = this.router.resolve(window.location.pathname)
+
 	    	console.log('JcApp __init')
 	    	return args
 		}
@@ -344,6 +349,94 @@ const App = function(superclass)
 		{
 			console.log('JcApp __behavior')
 			return args
+		}
+
+		// navigate(event)
+		// {
+		// 	if (event instanceof PopStateEvent) {
+		// 		event.preventDefault()
+		// 		this.view = this.router.resolve(event.state.pathname)
+			
+		// 	} else if (event instanceof PointerEvent) {
+		// 		event.preventDefault()
+		// 		this.view = event.currentTarget.tagName === 'A'
+		// 			? this.router.resolve(event.currentTarget.getAttribute('href'))
+		// 			: this.router.resolve(event.currentTarget.getAttribute('data-url'))
+			
+		// 	} else if (typeof event === 'string') {
+		// 		this.view = this.router.resolve(event)
+		// 	}
+			
+		// 	this.querySelector('cx-navigation').selectActiveItem(this.view.pathname)
+		// }
+
+		router = {
+
+			resolve(pathname)
+			{
+				console.log(pathname)
+				// let template = 'error-404'
+				// let parameters = {}
+
+				// for (const route of this.routes)
+				// {
+				// 	if (route.path.split('/').length < pathname.split('/').length) {
+				// 		continue
+					
+				// 	} else if (route.path === pathname) {
+				// 		template = route.template
+				// 		break
+				// 	} 
+
+				// 	const params = this._getRouteParams(route.path)
+
+				// 	if (Object.keys(params) > 0 && this._comparePaths(route.path, pathname, params)) {
+				// 		for (const [index, param] of Object.entries(params)) {
+				// 			parameters[param.name] = pathname.split('/')[index]
+				// 		}
+						
+				// 		template = route.template
+				// 		break
+				// 	}
+				// }
+
+				// return { 
+				// 	pathname: pathname, 
+				// 	template: template, 
+				// 	parameters: parameters 
+				// }
+			}// ,
+
+		// 	_getRouteParams(path)
+		// 	{
+		// 		let pathFragments = path.split('/')
+		// 		let routeParameters = {}
+
+		// 		pathFragments.forEach((fragment, index) => {
+		// 			if (fragment.charAt(0) === '{' && fragment.charAt(fragment.length-1) === '}') {
+		// 				let parameter = fragment.slice(1,-1)
+		// 				routeParameters[index] = parameter.charAt(parameter.length-1) === '?'
+		// 					? { nullable: true, name: parameter.slice(0,-1) }
+		// 					: { nullable: false, name: parameter }
+		// 			}
+		// 		})
+
+		// 		return routeParameters
+		// 	},
+
+		// 	_comparePaths(pathA, pathB, params)
+		// 	{
+		// 		for (let [index, fragment] of Object.entries(pathB.split('/')))
+		// 		{
+		// 			if (fragment === pathA.split('/')[index] || params[index]?.nullable) {
+		// 				continue
+		// 			}
+
+		// 			return false
+		// 		}
+				
+		// 		return true
+		// 	}
 		}
 	}
 }
