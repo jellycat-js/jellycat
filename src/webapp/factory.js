8d7f279e4690e7be5646b2a08fe5e6b16ad5a8e9
cxxi <l.a_lumet@yahoo.fr>
2023-08-24 15:54:27 +0200

    little change add mobile detect

--- src/webapp/factory.js
+++ src/webapp/factory.js
@@ -95,6 +95,12 @@ export default function(superclass)
 			}
 		}
 
+		isMobile()
+		{
+			const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+			return regex.test(navigator.userAgent)
+		}
+
 		router = {
 
 			resolve(pathname)
